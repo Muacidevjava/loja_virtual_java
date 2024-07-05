26b946bc70f8e6bea6cf3a8f5703b50e59be7103
muacidevjava <muaci@yahoo.com>
2024-07-05 13:05:58 -0300

    Adicionei Gett e Sett

--- src/main/java/com/example/demo/model/Pessoa.java
+++ src/main/java/com/example/demo/model/Pessoa.java
@@ -1,10 +1,11 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import org.springframework.core.SpringVersion;
 
 import java.io.Serializable;
-
+@Data
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @SequenceGenerator(name = "seq_pessoa", sequenceName = "seq_pessoa", initialValue = 1, allocationSize = 1)
