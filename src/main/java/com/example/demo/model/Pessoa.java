18965b561a6fc314c1260438829bad8b2d02f195
muacidevjava <muaci@yahoo.com>
2024-07-05 12:36:50 -0300

    criado a classe Pessoa.java

---
+++ src/main/java/com/example/demo/model/Pessoa.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import org.springframework.core.SpringVersion;
+
+import java.io.Serializable;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@SequenceGenerator(name = "seq_pessoa", sequenceName = "seq_pessoa", initialValue = 1, allocationSize = 1)
+public abstract class Pessoa implements Serializable {
+    private static final long serialVersionUID = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_pessoa")
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+
+}
