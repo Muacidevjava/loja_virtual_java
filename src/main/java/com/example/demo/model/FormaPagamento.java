66a5bb427fb04769de70f372c0a693a946f509b6
muacidevjava <muaci@yahoo.com>
2024-07-10 20:39:47 -0300

    criado FormaPagamento

---
+++ src/main/java/com/example/demo/model/FormaPagamento.java
@@ -0,0 +1,43 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "forma_pagamento")
+@SequenceGenerator(name = "seq_forma_pagamento", sequenceName = "seq_forma_pagamento", initialValue = 1, allocationSize = 1)
+
+public class FormaPagamento implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;
+
+
+    @Id
+    @SequenceGenerator(name = "seq_forma_pagamento", sequenceName = "seq_forma_pagamento")
+    private Long id;
+    private String descricao;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FormaPagamento that = (FormaPagamento) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
