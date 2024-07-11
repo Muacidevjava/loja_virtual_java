68b3e57b7c937f91845915df768770d22e9f078d
muacidevjava <muaci@yahoo.com>
2024-07-10 22:13:29 -0300

    criado a classe NotaFiscalCompra

---
+++ src/main/java/com/example/demo/model/NotaFiscalCompra.java
@@ -0,0 +1,51 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "nota_fiscal_compra")
+@SequenceGenerator(name = "nota_fiscal_compra", sequenceName = "nota_fiscal_compra", initialValue = 1, allocationSize = 1)
+public class NotaFiscalCompra implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "nota_fiscal_compra")
+    private Long id;
+
+    private String numeroNota;
+    private String serieNota;
+    private String descricaoObs;
+    private BigDecimal valorTotal;
+    private BigDecimal valorDesconto;
+    private BigDecimal valorIcms;
+
+
+    @Temporal(TemporalType.DATE)
+    private BigDecimal valorDataCompra;
+
+
+    @ManyToOne
+    @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk" ))
+    private Pessoa pessoa;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_pagar_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "Conta_pagar_fk" ))
+    private ContaPagar contaPagar;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NotaFiscalCompra that = (NotaFiscalCompra) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
