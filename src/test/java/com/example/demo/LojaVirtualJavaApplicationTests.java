6978d4405007b6b4226c7bdf4396036e26f4f42e
muacidevjava <muaci@yahoo.com>
2024-07-11 18:24:06 -0300

    Apliccando testes

--- src/test/java/com/example/demo/LojaVirtualJavaApplicationTests.java
+++ src/test/java/com/example/demo/LojaVirtualJavaApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import com.example.demo.controller.AcessoController;
+import com.example.demo.model.Acesso;
+import com.example.demo.repository.AcessoRepository;
+import com.example.demo.service.AcessoService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LojaVirtualJavaApplicationTests {
+@SpringBootTest(classes = LojaVirtualJavaApplication.class)
+ public class LojaVirtualJavaApplicationTests {
 
+	@Autowired
+	private AcessoService acessoService;
+
+	@Autowired
+	private AcessoController acessoController;
+
+	@Autowired
+	private AcessoRepository acessoRepository;
 	@Test
-	void contextLoads() {
+	public void testeCadastraAcesso() {
+		Acesso  acesso = new Acesso();
+		acesso.setDescrição("ROLE_ADMIN");
+
+		acessoService.save(acesso);
 	}
 
 }
