ffb2f0156cb7b1960a7d747e6becb96ea3696235
muacidevjava <muaci@yahoo.com>
2024-07-11 11:28:19 -0300

    criado AvaliacaoProduto

--- src/main/java/com/example/demo/model/AvaliacaoProduto.java
+++ src/main/java/com/example/demo/model/AvaliacaoProduto.java
@@ -3,6 +3,8 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.Objects;
+
 @Entity
 @Table(name = "avaliacao_produto")
 @SequenceGenerator(name = "seq_avaliacao_produto",sequenceName = "seq_avaliacao_produto", allocationSize = 1 , initialValue = 1)
@@ -15,6 +17,8 @@ public class AvaliacaoProduto implements Serializable {
 
     private Integer nota;
 
+    private String descricao;
+
     @ManyToOne
     @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk" ))
     private Pessoa pessoa;
@@ -23,4 +27,56 @@ public class AvaliacaoProduto implements Serializable {
     @JoinColumn(name = "produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk" ))
     private Produto produto;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public String getDescriacao() {
+        return descricao;
+    }
+
+    public void setDescriacao(String descriacao) {
+        this.descricao = descriacao;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AvaliacaoProduto that = (AvaliacaoProduto) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
